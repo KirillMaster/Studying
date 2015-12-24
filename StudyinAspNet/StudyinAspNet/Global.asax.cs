f85a98843cf8e1b698025755b8563aea26a04984
KirillMaster <suhorukih@mail.ru>
2015-12-24 22:24:07 +0300

    modified structure

--- StudyinAspNet/StudyinAspNet/Global.asax.cs
+++ StudyinAspNet/StudyinAspNet/Global.asax.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using StudyinAspNet.Models;
 
 namespace StudyinAspNet
 {
@@ -12,6 +10,7 @@ namespace StudyinAspNet
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new BookDbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
